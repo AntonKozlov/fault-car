2aca9eb71b660afcd892cc70269b9d1e1b6a61e4
Anton Kozlov <drakon.mega@gmail.com>
2015-06-08 22:23:46 +0300

    Add plotter

---
+++ plot.py
@@ -0,0 +1,17 @@
+#!/usr/bin/env python2
+
+import numpy as np
+import matplotlib.pyplot as plt
+import pylab
+
+import util
+
+def load(file):
+    return [ int(x) for x in file.read().splitlines() ]
+
+def plot(data):
+    plt.plot(data)
+    pylab.show()
+
+if __name__ == '__main__':
+    plot(load(util.input_file()))
