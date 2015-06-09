d247d3b076823c267ab16b9706c4a109652d5c55
Anton Kozlov <drakon.mega@gmail.com>
2015-06-09 23:44:49 +0300

    Move load/write file functions to util

--- fault_detect.py
+++ fault_detect.py
@@ -1,6 +1,7 @@
 #!/usr/bin/env python2
 
 import sys
+from util import load_data, write_data
 
 def moving_average(data, window_size):
 	out_data = []
@@ -9,11 +10,6 @@ def moving_average(data, window_size):
 		tmp += data[i] - data[i - window_size + 1]
 		out_data.append(tmp / window_size)
 	return out_data
-
-def log_into_file(data, file_out):
-	f = open(file_out, "w")
-	f.writelines([ "%.1f" % i + "\n" for i in data ])
-	f.close()
 
 # http://people.irisa.fr/Michele.Basseville/kniga/kniga.pdf
 # Example 2.3.4
@@ -24,18 +20,13 @@ def GLR_detector(data):
 def mean_value_detector(data):
 	pass
 
-def load_data(fname):
-	f = open(fname, "r")
-	return [ float(x) for x in f.read().splitlines() ]
-
-
 def main():
 	window_sz = 5
 	
 	filename = sys.argv[1]
 	data_in = load_data(filename)
 	filtered_data = moving_average(data_in, window_sz)
-	log_into_file(filtered_data, filename + ".out")
+	write_data(filtered_data, filename + ".out")
 
 	mean_value_detector(filtered_data)
 
--- util.py
+++ util.py
@@ -1,5 +1,13 @@
 import sys
 
+def load_data(file):
+    with open(file, "r") as f:
+        return [ float(x) for x in f.read().splitlines() ]
+
+def write_data(data, file_out):
+    with open(file_out, "w") as f:
+        f.writelines([ "%.1f" % i + "\n" for i in data ])
+
 def input_file():
     if len(sys.argv) < 2 or sys.argv[1] == "-":
         return sys.stdin
