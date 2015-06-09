eebfe499c197d09a999601f4a7f03997e8ea4717
Anton Kozlov <drakon.mega@gmail.com>
2015-06-09 23:46:09 +0300

    Add several graphics support to plot

--- plot.py
+++ plot.py
@@ -1,17 +1,16 @@
 #!/usr/bin/env python2
 
-import numpy as np
+import sys
+
 import matplotlib.pyplot as plt
 import pylab
 
 import util
 
-def load(file):
-    return [ float(x) for x in file.read().splitlines() ]
-
-def plot(data):
-    plt.plot(data)
+def plot(*datas):
+    for data in datas:
+        plt.plot(data)
     pylab.show()
 
 if __name__ == '__main__':
-    plot(load(util.input_file()))
+    plot(*[util.load_data(f) for f in sys.argv[1:]])
