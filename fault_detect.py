e76cf9ee8372a0448e8a8c32019a1c8dda83f7d7
Vita Loginova <vita.loginova@gmail.com>
2015-06-10 10:53:17 +0300

    Filter data via moving average with threshold

--- fault_detect.py
+++ fault_detect.py
@@ -17,18 +17,20 @@ def GLR_detector(data):
 	pass
 
 # Determine fault by change in mean
-def mean_value_detector(data):
-	pass
+def mean_value_detector(data, threshold):
+	return [0 if abs(x) < threshold else 1 for x in data]
 
 def main():
-	window_sz = 5
-	
 	filename = sys.argv[1]
 	data_in = load_data(filename)
-	filtered_data = moving_average(data_in, window_sz)
-	write_data(filtered_data, filename + ".out")
 
-	mean_value_detector(filtered_data)
+	# moving average
+	threshold = 3000
+	window_sz = 50
+
+	filtered_data = moving_average(data_in, window_sz)
+	res = mean_value_detector(filtered_data, threshold)
+	write_data(res, filename + ".out")
 
 if __name__ == "__main__":
 	main()
